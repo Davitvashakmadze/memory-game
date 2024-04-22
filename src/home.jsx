4a4fbcb9f3a3c6f409f36c38df73f0461397285f
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-04-22 23:15:16 +0400

    feat: add main app starting page and buttons 30%

--- src/home.jsx
+++ src/home.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function home() {
   return (
     <div>
-        <div className='main-app-wrapper'>
-            <div className='Select-Theme'>
-                <button>Numbers</button>
-                <button>Icons</button>
-            </div>
-            <div className='num-players'></div>
-            <div className='grid-size'></div>
-            <button className='start'>Start Game</button>
+      <div className="main-app-wrapper">
+        <div className="Select-Theme">
+          <label htmlFor="">Select Theme</label>
+          <div className="btn-wrapper">
+            <button>Numbers</button>
+            <button>Icons</button>
+          </div>
         </div>
+        <div className="num-players"></div>
+        <div className="grid-size"></div>
+        <button className="start">Start Game</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default home
+export default home;
