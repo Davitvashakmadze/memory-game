3a766f75868986014324fb1b724da8edd2b1b2dd
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-04-23 23:32:11 +0400

    feat: starting page buttons with css 100%

--- src/home.jsx
+++ src/home.jsx
@@ -11,8 +11,22 @@ function home() {
             <button>Icons</button>
           </div>
         </div>
-        <div className="num-players"></div>
-        <div className="grid-size"></div>
+        <div className="num-players">
+        <label htmlFor="">Numbers of Players</label>
+          <div className="btn-wrapper">
+            <button>1</button>
+            <button>2</button>
+            <button>2</button>
+            <button>3</button>
+          </div>
+        </div>
+        <div className="grid-size">
+        <label htmlFor="">Grid Size</label>
+          <div className="btn-wrapper">
+            <button>4x4</button>
+            <button>6x6</button>
+          </div>
+        </div>
         <button className="start">Start Game</button>
       </div>
     </div>
