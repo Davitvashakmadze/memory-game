4fb081e386197ce5bf1ec0cc754b514918342e70
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-04-24 23:57:44 +0400

    feat: add some css style

--- src/home.jsx
+++ src/home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function home() {
   return (
     <div>
+      <h1>Memory Game</h1>
       <div className="main-app-wrapper">
         <div className="Select-Theme">
           <label htmlFor="">Select Theme</label>
@@ -12,7 +13,7 @@ function home() {
           </div>
         </div>
         <div className="num-players">
-        <label htmlFor="">Numbers of Players</label>
+          <label htmlFor="">Numbers of Players</label>
           <div className="btn-wrapper">
             <button>1</button>
             <button>2</button>
@@ -21,7 +22,7 @@ function home() {
           </div>
         </div>
         <div className="grid-size">
-        <label htmlFor="">Grid Size</label>
+          <label htmlFor="">Grid Size</label>
           <div className="btn-wrapper">
             <button>4x4</button>
             <button>6x6</button>
