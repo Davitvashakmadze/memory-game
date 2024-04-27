b3c10cda5d9a4370c1514f5944c00fcd20f08d32
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-04-27 23:57:50 +0400

    feat: add next page header

--- src/pages/Gaming.jsx
+++ src/pages/Gaming.jsx
@@ -4,7 +4,11 @@ const Gaming = () => {
   return (
     <div>
       <div className="main-app-wrapper">
-        
+        <div className="app-header">
+          <h1>Memory</h1>
+          <button>Restart</button>
+          <button className="">New Game</button>
+        </div>
       </div>
     </div>
   );
