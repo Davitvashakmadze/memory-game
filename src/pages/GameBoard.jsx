e04eae48499e338a6a765ccb3d5a199f7f926277
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-04-30 22:32:35 +0400

    feat: add start game btn function

--- src/pages/GameBoard.jsx
+++ src/pages/GameBoard.jsx
@@ -1,44 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Gaming from "./Gaming";
 
-
-
 function GameBoard() {
+  const [gameStarted, setGameStarted] = useState(false);
 
   const handleClickStartGame = () => {
-    console.log("Start Game")
-  }
-  
+    console.log("Start Game");
+    setGameStarted(true);
+  };
+
   return (
     <div>
-      <h1>Memory Game</h1>
-      <div className="main-app-wrapper">
-        <div className="Select-Theme">
-          <label htmlFor="">Select Theme</label>
-          <div className="btn-wrapper">
-            <button className="active">Numbers</button>
-            <button>Icons</button>
+      {gameStarted ? (
+        <Gaming />
+      ) : (
+        <div>
+          <h1>Memory Game</h1>
+          <div className="main-app-wrapper">
+            <div className="Select-Theme">
+              <label htmlFor="">Select Theme</label>
+              <div className="btn-wrapper">
+                <button className="active">Numbers</button>
+                <button>Icons</button>
+              </div>
+            </div>
+            <div className="num-players">
+              <label htmlFor="">Numbers of Players</label>
+              <div className="btn-wrapper">
+                <button className="active">1</button>
+                <button>2</button>
+                <button>2</button>
+                <button>3</button>
+              </div>
+            </div>
+            <div className="grid-size">
+              <label htmlFor="">Grid Size</label>
+              <div className="btn-wrapper">
+                <button className="active">4x4</button>
+                <button>6x6</button>
+              </div>
+            </div>
+            <button className="start" onClick={handleClickStartGame}>Start Game</button>
           </div>
         </div>
-        <div className="num-players">
-          <label htmlFor="">Numbers of Players</label>
-          <div className="btn-wrapper">
-            <button className="active">1</button>
-            <button>2</button>
-            <button>2</button>
-            <button>3</button>
-          </div>
-        </div>
-        <div className="grid-size">
-          <label htmlFor="">Grid Size</label>
-          <div className="btn-wrapper">
-            <button className="active">4x4</button>
-            <button>6x6</button>
-          </div>
-        </div>
-        <button className="start" onClick={handleClickStartGame}>Start Game</button>
-      </div>
-      <Gaming/>
+      )}
     </div>
   );
 }
--- src/pages/Gaming.jsx
+++ src/pages/Gaming.jsx
@@ -3,12 +3,15 @@ import React from "react";
 const Gaming = () => {
   return (
     <div>
-      <div className="main-app-wrapper">
+      <div className="gaming-wrapper">
         <div className="app-header">
           <h2>Memory</h2>
-          <button className="btn">Restart</button>
-          <button className="btn">New Game</button>
+          <div className="btns">
+            <button className="btn restart">Restart</button>
+            <button className="btn newGame">New Game</button>
+          </div>
         </div>
+        <div className="main-app-wrapper"></div>
       </div>
     </div>
   );
