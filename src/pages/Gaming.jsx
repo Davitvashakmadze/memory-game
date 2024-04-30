f0e0c4eb466ac6aeb21f978eb636251fbb56acdf
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-04-30 23:02:59 +0400

    feat: add start game btn function 15%

--- src/pages/Gaming.jsx
+++ src/pages/Gaming.jsx
@@ -11,7 +11,14 @@ const Gaming = () => {
             <button className="btn newGame">New Game</button>
           </div>
         </div>
-        <div className="main-app-wrapper"></div>
+        <div className="main-app-wrapper">
+          <div className="grid-container">
+            {/* Create 4x4 grid */}
+            {[...Array(16)].map((_, index) => (
+              <div key={index} className="grid-item"></div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
